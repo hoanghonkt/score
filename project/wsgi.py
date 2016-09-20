942a35f7e26ae4549a41d93bb1e542f9bc1e9d71
khoi nguyen <todinhthi@gmail.com>
2016-09-20 16:56:58 +0700

    Update logic

--- project/wsgi.py
+++ project/wsgi.py
@@ -7,7 +7,13 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
+
+# add the hellodjango project path into the sys.path
+sys.path.append('/opt/python/current/app/project')
+
+# add the virtualenv site-packages path to the sys.path
+sys.path.append("/opt/python/run/venv/lib/python2.7/site-packages")
 
 from django.core.wsgi import get_wsgi_application
 
